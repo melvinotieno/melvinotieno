fa2abfb1bc9d801970e6e125d4d9869d8637be0c
Melvin Otieno <o.melvinotieno@gmail.com>
2024-08-24 10:51:33 +0300

    feat: Add Heading component for pages with animated text display

---
+++ src/components/heading.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function Heading({
+  animate,
+  inaminate,
+}: Readonly<{ animate: string; inaminate?: string }>) {
+  const [animated, setAnimated] = useState(inaminate ? "" : animate.charAt(0));
+
+  useEffect(() => {
+    let currentIndex = inaminate ? 0 : 1;
+
+    const interval = setInterval(() => {
+      // Display a new character from the text to animate.
+      const char = animate.charAt(currentIndex);
+      setAnimated((prev) => prev + char);
+
+      currentIndex++;
+
+      // Clear interval when we've displayed the entire animated text.
+      if (currentIndex === animate.length) clearInterval(interval);
+    }, 150);
+
+    // We must clear the interval when the component unmounts.
+    return () => clearInterval(interval);
+  }, [animate, inaminate]);
+
+  return (
+    <h1 className="font-mono text-3xl font-bold">
+      {inaminate} {animated}
+    </h1>
+  );
+}
