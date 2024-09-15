03a2181c38152848ce7fa27562a864c4632a3fef
Melvin Otieno <o.melvinotieno@gmail.com>
2024-09-15 15:19:32 +0300

    fix: replace className with tw

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -8,9 +8,9 @@ export function GET(request: Request) {
 
   return new ImageResponse(
     (
-      <div className="flex h-full w-full flex-col items-center justify-center bg-white">
-        <div className="flex w-full flex-col justify-between p-8 px-4 py-12 md:flex-row md:items-center">
-          <h2 className="flex flex-col text-left text-4xl font-bold tracking-tight">
+      <div tw="flex h-full w-full flex-col items-center justify-center bg-white">
+        <div tw="flex w-full flex-col justify-between p-8 px-4 py-12 md:flex-row md:items-center">
+          <h2 tw="flex flex-col text-left text-4xl font-bold tracking-tight">
             {title}
           </h2>
         </div>
