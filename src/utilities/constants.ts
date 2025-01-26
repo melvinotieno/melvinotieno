901a75be0214fed019c587be4cdc48e076014c6d
Melvin Otieno <o.melvinotieno@gmail.com>
2025-01-26 15:13:47 +0300

    fix: remove unused constants from utilities

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,8 +1,3 @@
 export const BASE_URL = "https://melvinotieno.com";
-
 export const AUTHOR_NAME = "Melvin Otieno";
-
-// prettier-ignore
-export const BLOG_DESCRIPTION = "My thoughts on technology, software development, and more.";
-
 export const TWITTER_HANDLE = "@o_melvinotieno";
